7cf8250b598621c1a66853980a2eeb01d578176f
Julia Wheless <juliarwheless@gmail.com>
2025-04-28 15:36:51 -0700

    fixed 403 error when clicking on dates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,9 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/add-task/<date>')
-def add_task(date):
-    return render_template('add_date.html', date=date)
+@app.route('/add-task/<date>', methods=['GET', 'POST'])
+def add_task_date(date):
+    if request.method == 'POST':
+        task_name = request.form.get('task_name')
+        # Save the task along with the 'date'
+        return redirect(url_for('home'))
+    return render_template('add_task.html', date=date)
 
 
 
