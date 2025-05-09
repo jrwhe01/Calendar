b7dae4336f2a0863da5d7fe589776def5cc3ac46
Julia Wheless <juliarwheless@gmail.com>
2025-05-08 23:39:25 -0700

    add task (sort of)

--- app.py
+++ app.py
@@ -6,12 +6,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+tasks_by_date = {}
+
 @app.route('/add-task/<date>', methods=['GET', 'POST'])
 def add_task_date(date):
     if request.method == 'POST':
         task_name = request.form.get('task_name')
-        # Save the task along with the 'date'
-        return redirect(url_for('home'))
+        if task_name:
+            tasks_by_date.setdefault(date, []).append(task_name)
+        return redirect(url_for('index'))
     return render_template('add_task.html', date=date)
 
 
